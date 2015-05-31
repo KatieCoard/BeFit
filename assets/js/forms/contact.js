db424a94ea6b56ca3cf2aedad7444290d2fc06ae
RotaryVictoria <rotarydowntownvictoria@gmail.com>
2015-05-31 15:01:02 -0700

    moved files from html folder to root folder

---
+++ assets/js/forms/contact.js
@@ -0,0 +1,67 @@
+var ContactForm = function () {
+
+    return {
+        
+        //Contact Form
+        initContactForm: function () {
+	        // Validation
+	        $("#sky-form3").validate({
+	            // Rules for form validation
+	            rules:
+	            {
+	                name:
+	                {
+	                    required: true
+	                },
+	                email:
+	                {
+	                    required: true,
+	                    email: true
+	                },
+	                message:
+	                {
+	                    required: true,
+	                    minlength: 10
+	                },
+	                captcha:
+	                {
+	                    required: true,
+	                    remote: 'assets/plugins/sky-forms-pro/skyforms/captcha/process.php'
+	                }
+	            },
+	                                
+	            // Messages for form validation
+	            messages:
+	            {
+	                name:
+	                {
+	                    required: 'Please enter your name',
+	                },
+	                email:
+	                {
+	                    required: 'Please enter your email address',
+	                    email: 'Please enter a VALID email address'
+	                },
+	                message:
+	                {
+	                    required: 'Please enter your message'
+	                },
+	                captcha:
+	                {
+	                    required: 'Please enter characters',
+	                    remote: 'Correct captcha is required'
+	                }
+	            },
+	                                
+	            
+	            // Do not change code below
+	            errorPlacement: function(error, element)
+	            {
+	                error.insertAfter(element.parent());
+	            }
+	        });
+        }
+
+    };
+    
+}();
