3bb4e4c08004c078bacc05a5ae3522f72f001609
arcoard <arcoard+github@gmail.com>
2015-05-31 12:19:56 -0700

    Tonnes of work yeaaah

--- HTML/assets/js/forms/contact.js
+++ HTML/assets/js/forms/contact.js
@@ -53,21 +53,6 @@ var ContactForm = function () {
 	                }
 	            },
 	                                
-	            // Ajax form submition                  
-	            submitHandler: function(form)
-	            {
-	                $(form).ajaxSubmit(
-	                {
-	                    beforeSend: function()
-	                    {
-	                        $('#sky-form3 button[type="submit"]').attr('disabled', true);
-	                    },
-	                    success: function()
-	                    {
-	                        $("#sky-form3").addClass('submited');
-	                    }
-	                });
-	            },
 	            
 	            // Do not change code below
 	            errorPlacement: function(error, element)
